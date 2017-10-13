c7c351ac1472312f242ab16337c70e68c95adc4b
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-13 14:17:30 +0700

    contoh file

---
+++ src/main/java/id/zaura/marketplace/store/controller/HomeController.java
@@ -0,0 +1,11 @@
+package id.zaura.marketplace.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public void homepage(){}
+}
